2d9a448f120087f0ab0fe9a5ee76feb43ba9194a
JustTams <sarg.tier1@gmail.com>
2020-04-20 19:47:58 -0500

    url update

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://next-ecomm.now.sh'
+    ? 'https://next-ecomm-sargtier1.salvatore-argentieri.now.sh'
     : 'http://localhost:3000'
 
 export default baseUrl
